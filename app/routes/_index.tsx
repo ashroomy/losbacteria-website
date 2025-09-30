787f023201081e80d266af2a92878dd324533490
Andrea <andre@Andreas-MacBook-Air.local>
2025-09-30 09:16:16 -0700

    Fix sizing spacing for mobile

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -62,7 +62,7 @@ export default function IndexPage() {
   return (
     <Layout>
       <main className="container mx-auto  pb-[32px]">
-        <ul className="grid xs:grid-cols-2 md:grid-cols-4  max-w-3xl m-auto md:px-[0] md:gap-[45px] xs:gap-[40px] xs:px-[30px]">
+        <ul className="grid xs:grid-cols-2 md:grid-cols-4  max-w-3xl m-auto md:gap-[45px] xs:gap-[40px]">
           {products.map((product, index) => {
             const cssStyle = cssHovers[index % cssHovers.length];
             return (
